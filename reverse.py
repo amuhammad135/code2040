4453077fe58d33afb2b4ed46b430ade048888568
amuhammad135 <am4411@columbia.edu>
2016-10-25 01:29:00 -0400

    Update reverse.py

--- reverse.py
+++ reverse.py
@@ -3,6 +3,7 @@ import requests
 r = requests.post('http://challenge.code2040.org/api/reverse'
 , data ={'token' : '532856fe705c8a089d3e4b8213327687'})
 
+#reverses string
 rev = r.text[::-1]
 
 r = requests.post('http://challenge.code2040.org/api/reverse/validate'

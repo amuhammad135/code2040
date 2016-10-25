cf43947eee637de909cd8916745891a23eed1dc0
amuhammad135 <am4411@columbia.edu>
2016-10-25 01:37:15 -0400

    Update register.py

--- register.py
+++ register.py
@@ -1,3 +1,9 @@
+'''
+Abdurrauf Muhammad
+
+register.py
+'''
+
 import requests
 
 r = requests.post('http://challenge.code2040.org/api/register', data ={'token' : '532856fe705c8a089d3e4b8213327687' , 'github' :  'https://github.com/amuhammad135/code2040'})

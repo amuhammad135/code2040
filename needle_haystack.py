b4e5dadfa55102b0d3c55f15a7ea2b5e8a240096
amuhammad135 <am4411@columbia.edu>
2016-10-25 01:48:56 -0400

    Update needle_haystack.py

--- needle_haystack.py
+++ needle_haystack.py
@@ -9,13 +9,8 @@ import requests
 r = requests.post('http://challenge.code2040.org/api/haystack'
 , data ={'token' : '532856fe705c8a089d3e4b8213327687'})
 
-#dictionary given
 dict = r.json()
-
-#string needed to find in array
 needle = dict.get('needle')
-
-#array to be iterated through
 haystack = dict.get('haystack')
 
 #iterates through array for position of needle

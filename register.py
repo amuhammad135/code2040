52e910dcfbc4b972eaa828578baf643bf965e17f
AR Muhammad <armuhammad@dyn-209-2-227-248.dyn.columbia.edu>
2016-10-24 22:55:09 -0400

    Adding code2040 files

---
+++ register.py
@@ -0,0 +1,4 @@
+import requests
+
+r = requests.post('http://challenge.code2040.org/api/register', data ={'token' : '532856fe705c8a089d3e4b8213327687' , 'github' :  'https://github.com/amuhammad135/code2040'})
+
